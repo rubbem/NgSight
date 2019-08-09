7ccd0ae949434649177c91de849b8a8da642b66b
Rubem <rubem81@gmail.com>
2019-08-08 21:21:41 -0300

    Add pie and line chart

--- src/app/charts/line-chart/line-chart.component.ts
+++ src/app/charts/line-chart/line-chart.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { LINE_CHART_COLORS } from '../../shared/chart.colors';
+
+const LINE_CHART_SAMPLE_DATE: any[] = [
+  { data: ['32','14','46','54','24','49'], label:'Sentiment Analysis'},
+  { data: ['11','15','23','28','37','28'], label:'Image Recognition'},
+  { data: ['12','35','45','12','42','28'], label:'Forecasting'}
+];
+
+const LINE_CHART_LABELS: string[] = ['Jan','Fev','Mar','Abr','Mai','Jun'];
 
 @Component({
   selector: 'app-line-chart',
@@ -8,6 +17,15 @@ import { Component, OnInit } from '@angular/core';
 export class LineChartComponent implements OnInit {
 
   constructor() { }
+
+  public lineChartData: any[] = LINE_CHART_SAMPLE_DATE;
+  public lineChartLabels: string[] = LINE_CHART_LABELS;
+  public lineChartType = 'line';
+  public lineChartLegend = true;
+  public lineChartColors = LINE_CHART_COLORS;
+  public lineChartOptions: any = {
+    responsive: true
+  };
 
   ngOnInit() {
   }
--- src/app/charts/pie-chart/pie-chart.component.ts
+++ src/app/charts/pie-chart/pie-chart.component.ts
@@ -9,6 +9,15 @@ export class PieChartComponent implements OnInit {
 
   constructor() { }
 
+  public pieChartData: number[] = [350, 450, 120];
+  public pieChartLabels: string[] = ['XYZ Logistics','Main St','Acme Hosting'];
+  public colors: any = [
+    {
+      backgroundColor: ['#26547c','#ff6b6b','#ffd166']
+    }
+  ];
+  public pieChartType = 'pie';
+
   ngOnInit() {
   }
 
