4b7a0d6b424cad7d100ed19680fdaa7e542ff971
Rubem <rubem81@gmail.com>
2019-08-12 20:17:21 -0300

    Create Server Component

--- src/app/sections/section-orders/section-orders.component.ts
+++ src/app/sections/section-orders/section-orders.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from '../../shared/order';
 
 @Component({
   selector: 'app-section-orders',
@@ -8,6 +9,22 @@ import { Component, OnInit } from '@angular/core';
 export class SectionOrdersComponent implements OnInit {
 
   constructor() { }
+
+  orders: Order[] = [
+    { id: 1, customer:
+      {id: 1, name: 'Mains st', state: 'California', email: 'teste@gmail.com'},
+       total: 230, placed: new Date( 2017, 12, 1), fulfilled: new Date(2017, 12, 2)},
+    { id: 2, customer:
+        {id: 2, name: 'Mains st', state: 'California', email: 'teste@gmail.com'},
+         total: 230, placed: new Date( 2017, 12, 1), fulfilled: new Date(2017, 12, 2)},
+    { id: 3, customer:
+          {id: 2, name: 'Mains st', state: 'California', email: 'teste@gmail.com'},
+           total: 230, placed: new Date( 2017, 12, 1), fulfilled: new Date(2017, 12, 2)},
+    { id: 4, customer:
+            {id: 4, name: 'Mains st', state: 'California', email: 'teste@gmail.com'},
+             total: 230, placed: new Date( 2017, 12, 1), fulfilled: new Date(2017, 12, 2)}
+   
+  ];
 
   ngOnInit() {
   }
